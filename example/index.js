26411e40381bac7bb2ff8caabf92e7fcb9343047
Matthew Wear <matthew.wear@gmail.com>
2020-07-21 16:01:18 -0700

    chore: update example after syncing with upstream changes

--- example/index.js
+++ example/index.js
@@ -1,19 +1,17 @@
 const { lightstep, opentelemetry } = require('../build/src/index');
-const { LogLevel } = require('@opentelemetry/core');
 
 // set access token or use LS_ACCESS_TOKEN environment variable
 const token = 'YOUR ACCESS TOKEN';
 
 const sdk = lightstep.configureOpenTelemetry({
   token,
-  logLevel: LogLevel.DEBUG,
   serviceName: 'locl-ex',
 });
 
-sdk.start();
+sdk.start().then(() => {
+  const tracer = opentelemetry.trace.getTracer('locl-node-example');
+  const span = tracer.startSpan('test-span');
+  span.end();
 
-const tracer = opentelemetry.trace.getTracer('locl-node-example');
-const span = tracer.startSpan('test-span');
-span.end();
-
-opentelemetry.trace.getTracerProvider().getActiveSpanProcessor().shutdown();
+  opentelemetry.trace.getTracerProvider().getActiveSpanProcessor().shutdown();
+});
